54828f2dac6bb8516fa90105817b49348904cab7
chimeng <chimeng@gomeholdings.com>
2017-11-22 13:47:50 +0800

    heap sort

--- sort/src/sort/Sort.java
+++ sort/src/sort/Sort.java
@@ -2,6 +2,8 @@ package sort;
 
 import sort.bubble.Bubble;
 import sort.divide.DivideAndConquer;
+import sort.heapify.HeapSort;
+import sort.heapify.Heapify;
 import sort.insertion.InsertionSort;
 
 /**
@@ -11,26 +13,37 @@ import sort.insertion.InsertionSort;
 
 public class Sort {
 
-    static int[] arr = {5,3,4,7,1,8,2,6,0};
+    //  static int[] arr = {5,3,4,7,1,8,2,6,0};
+    static int[] arr = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 1, 0, 2, 20};
+    ;
+
     //static int[] arr = {11,12,1,14,15,5,6,7,8,9,10,1,3,0,0,0,0};
     //static int[] arr = {8,9,11,10,0,1,2,1};
     public static void main(String[] args) {
 
-      //  Bubble.sort(arr);
-        //InsertionSort.sort(arr);
-        int r = arr.length-1;
-        int p = 0;
-        int q = (r+p)/2;
-        print();
-        DivideAndConquer.mergeSort(arr,p,r);
+        {
+            Bubble.sort(arr);
+        }
+        {
+            InsertionSort.sort(arr);
+        }
+        {
+            int r = arr.length - 1;
+            int p = 0;
+            DivideAndConquer.mergeSort(arr, p, r);
+        }
+
+        {
+            HeapSort.sort(arr);
+        }
         print();
 
     }
 
-    static void print(){
+    static void print() {
 
-        for (int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+"  ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "  ");
         }
         System.out.println();
     }
